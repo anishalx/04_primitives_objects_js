e5dfe68000782c76b8727085abf9b25aa029812a
Anish <113669810+radlysamar@users.noreply.github.com>
2022-09-29 17:31:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// nn bb ss u //primitives in js
+let a = null
+let b = 345
+let c = true // or false 
+let d = BigInt("10") + BigInt("20")
+let e = "anish"
+let f = Symbol("hello world")
+let g = undefined
+let h =
+  console.log(a, b, c, d, e, f, g)
+console.log(typeof g)
+
+// Non primitive datatypes objects in js
+const items = {
+  "anish": true,
+  "sambhu": false,
+  "Saurabh": 45,
+  "Ravi": undefined
+
+}
+console.log(items)
+console.log(items["anish"])
